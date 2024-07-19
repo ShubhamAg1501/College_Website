911e58e3da860f3f21c0589ea8a627cdfeb426ae
Shubham <shubhamagrawal1501@gmail.com>
2024-07-20 02:35:39 +0530

    add routes and create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/Sign-in" element={<SignIn/>} />
+      <Route path="/Sign-up" element={<SignUp/>} />
+      <Route path="/Profile" element={<Profile/>} />
+    </Routes>
+    
+    </BrowserRouter>
   )
 }
