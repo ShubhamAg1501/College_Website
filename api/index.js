0e939c7e83df5d0dce84c8b79c8360e272821cb6
Shubham <shubhamagrawal1501@gmail.com>
2024-07-21 00:51:56 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'; // installed it using npm i dotenv to securely store the password of the DB
+dotenv.config();  // initialize dotenv
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('Connected to MongoDB');
+}).catch((err)=>{
+    console.log('err');
+});
 
 const app=express();
 
