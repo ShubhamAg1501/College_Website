aad31fc9ccc7fdcc1c7d704290fe464bdf158dfd
Shubham <shubhamagrawal1501@gmail.com>
2024-07-22 02:49:12 +0530

    create ssignup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'; // installed it using npm i dotenv to securely store the password of the DB
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();  // initialize dotenv
 
@@ -14,9 +15,12 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 const app=express();
 
+app.use(express.json());
+
 app.listen(3000,()=>{
     console.log('Server listening on port 3000');
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
